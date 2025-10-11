8423744db6f63f3dd4ec209af599c9bb9e9bef3d
appfathussalafi-cmyk <appfathussalafi@gmail.com>
2025-10-11 07:19:29 +0700

    Update supabaseClient.ts

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://xlkphzmjbfyzpiqnnyvc.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhsa3Boem1qYmZ5enBpcW5ueXZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA2NzI3OTIsImV4cCI6MjAzNjI0ODc5Mn0.S-8vyAMan26t0c_3r_i5M24A5f26a1L3s6g2w5pY5wE';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhsa3Boem1qYmZ5enBpcW5ueXZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAwNzk4OTMsImV4cCI6MjA3NTY1NTg5M30.WMOI-22Maro_NtfQff9V7irqy2eRTy8lPtSljh3vLrs';
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Supabase URL and anon key are required.');
